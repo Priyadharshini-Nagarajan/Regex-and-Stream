0103c4f4bb1e0d78b9956a0def4c9db67fc7f3ac
Priyadharshini-Nagarajan <161636455+Priyadharshini-Nagarajan@users.noreply.github.com>
2024-03-05 14:01:26 +0530

    Update Storeinstream.java

--- NewOneFolder/Storeinstream.java
+++ NewOneFolder/Storeinstream.java
@@ -1,1 +1,19 @@
+package NewOneFolder;
 
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Storeinstream {
+    public static void main(String[] args) {
+        List<Integer> num = Arrays.asList(1,2,3,4,5,6,7,8);
+        int result= num.stream()
+                        .filter(n-> n%2==0)
+                        
+                        .map(n-> n*2)
+                        .reduce(0, (c,e)->c+e);
+        System.out.println(result);
+
+    }
+
+}
