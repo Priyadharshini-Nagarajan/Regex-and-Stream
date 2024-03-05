e717a4673a08e7cdf0ac39c40730a29c02a81a21
Priyadharshini-Nagarajan <161636455+Priyadharshini-Nagarajan@users.noreply.github.com>
2024-03-05 13:55:00 +0530

    Add files via upload

--- NewOneFolder/Validation.java
+++ NewOneFolder/Validation.java
@@ -1,1 +1,30 @@
+package NewOneFolder;
 
+import java.util.Scanner;
+
+public class Validation {
+    public static boolean valid(String input){
+        return input.matches("^[a-zA-Z\\s]+$");
+    }
+    public static void main(String[] args) {
+
+       
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a String");
+        String n=sc.nextLine();
+        int num=n.length();
+        sc.close();
+        if(num<=5){
+        if(valid(n)){
+            System.out.println("valid");
+        }
+        else{
+            System.out.println("not valid");
+        }
+    }
+    else{
+        System.out.println("not valid");
+    }
+    }
+
+}
